0838b43297ab1ab95dea28f1a7cc989a309fcb60
jakubfiala <j4kub.fiala@gmail.com>
2024-06-17 12:00:10 +0200

    add bottom margin to bars canvas

--- renderer.js
+++ renderer.js
@@ -1,9 +1,11 @@
 import { PCFSoftShadowMap, WebGLRenderer, ACESFilmicToneMapping } from 'three';
 
+const BOTTOM_MARGIN = 50;
+
 export default () => {
   const renderer = new WebGLRenderer({ antialias: true, alpha: true, canvas: document.getElementById('bars') });
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight + BOTTOM_MARGIN);
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = PCFSoftShadowMap;
   renderer.toneMapping = ACESFilmicToneMapping;
